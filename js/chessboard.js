dfc1029b575656a28c620d5a14519e2d8a044df3
nolann7 <nolann709@gmail.com>
2023-06-01 11:00:24 +0300

    draw function

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,20 +1,30 @@
 export default {
-	draw,
-	highlight
+  draw,
+  highlight,
 };
-
 
 // ****************************
 
 function draw(boardEl) {
-	// TODO: draw the chessboard, 8 rows (divs)
-	// of 8 tiles (divs) each, inserting all DOM
-	// elements into `boardEl` div
+  // TODO: draw the chessboard, 8 rows (divs)
+  // of 8 tiles (divs) each, inserting all DOM
+  // elements into `boardEl` div
+  
+  for (let i = 0; i < 8; i++) {
+    let row = document.createElement('div');
+    boardEl.append(row);
+
+    for (let j = 0; j < 8; j++) {
+      let tile = document.createElement('div');
+      row.append(tile);
+    }
+  }
+  console.log(boardEl);
 }
 
 function highlight(tileEl) {
-	// TODO: clear previous highlights (if any) and
-	// then find the tiles in the two diagonals
-	// (major and minor) that `tileEl` belongs to,
-	// to highlight them via CSS class "highlighted"
+  // TODO: clear previous highlights (if any) and
+  // then find the tiles in the two diagonals
+  // (major and minor) that `tileEl` belongs to,
+  // to highlight them via CSS class "highlighted"
 }
