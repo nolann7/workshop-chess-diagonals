b785e4f65b807c177500d224e5b431862f89adef
nolann7 <nolann709@gmail.com>
2023-06-01 12:28:04 +0300

    implementing full logic of exercise traversing board to find every legit diagonal tile

--- js/app.js
+++ js/app.js
@@ -1,32 +1,28 @@
-import Chessboard from "./chessboard.js";
+import Chessboard from './chessboard.js';
 
+document.addEventListener('DOMContentLoaded', async function ready() {
+  var boardEl = document.getElementById('board');
+  var clearHighlightsBtn = document.getElementById('clear-highlights-btn');
 
-document.addEventListener("DOMContentLoaded",async function ready(){
-	var boardEl = document.getElementById("board");
-	var clearHighlightsBtn = document.getElementById("clear-highlights-btn");
+  Chessboard.draw(boardEl);
 
-	Chessboard.draw(boardEl);
+  boardEl.addEventListener('click', onClickTile, false);
+  clearHighlightsBtn.addEventListener('click', clearHighlights, false);
 
-	boardEl.addEventListener("click",onClickTile,false);
-	clearHighlightsBtn.addEventListener("click",clearHighlights,false);
+  // ********************************
 
+  function onClickTile(evt) {
+    var clickedEl = evt.target;
+    // console.log(clickedEl.matches('#board > div > div'));
+    // clicked on a board tile?
+    if (clickedEl.matches('#board > div > div')) {
+      Chessboard.highlight(clickedEl);
+    } else {
+      clearHighlights();
+    }
+  }
 
-	// ********************************
-
-	function onClickTile(evt) {
-		var clickedEl = evt.target;
-
-		// clicked on a board tile?
-		if (clickedEl.matches("#board > div > div")) {
-			Chessboard.highlight(clickedEl);
-		}
-		else {
-			clearHighlights();
-		}
-	}
-
-	function clearHighlights() {
-		Chessboard.highlight();
-	}
-
+  function clearHighlights() {
+    Chessboard.highlight();
+  }
 });
--- js/chessboard.js
+++ js/chessboard.js
@@ -6,25 +6,79 @@ export default {
 // ****************************
 
 function draw(boardEl) {
-  // TODO: draw the chessboard, 8 rows (divs)
-  // of 8 tiles (divs) each, inserting all DOM
-  // elements into `boardEl` div
-  
   for (let i = 0; i < 8; i++) {
     let row = document.createElement('div');
+    row.dataset.row = i + 1;
     boardEl.append(row);
 
     for (let j = 0; j < 8; j++) {
       let tile = document.createElement('div');
+      tile.dataset.position = `${i + 1}-${j + 1}`;
       row.append(tile);
     }
   }
-  console.log(boardEl);
 }
 
-function highlight(tileEl) {
-  // TODO: clear previous highlights (if any) and
-  // then find the tiles in the two diagonals
-  // (major and minor) that `tileEl` belongs to,
-  // to highlight them via CSS class "highlighted"
+function highlight(tileEl = null) {
+  // clearing prev highlights:
+  const boardEl = document.getElementById('board');
+  const rows = boardEl.childNodes;
+  for (const row of rows) {
+    for (const tile of row.childNodes) {
+      tile.classList.remove('highlighted');
+    }
+  }
+  if (!tileEl) return;
+
+  const [currentRow, currentColumn] = tileEl.dataset.position
+    .split('-')
+    .map(val => +val);
+
+  const allCurrentDiagonalTiles = findAllDiagonals(currentRow, currentColumn);
+
+  for (let [curRow, curCol] of allCurrentDiagonalTiles) {
+    let position = `${curRow}-${curCol}`;
+    let currentTile = document.querySelector(`[data-position="${position}"]`);
+    currentTile.classList.add('highlighted');
+  }
+
+  function findAllDiagonals(currentRow, currentColumn) {
+    const allPositions = [];
+    findRecursive(currentRow, currentColumn, allPositions, 'main');
+    return allPositions;
+  }
+
+  function findRecursive(currentRow, currentColumn, allPositions, path) {
+    allPositions.push([currentRow, currentColumn]);
+
+    const dNeighbors = {};
+    if (
+      (path == 'main' || path == 'topLeft') &&
+      currentRow > 1 &&
+      currentColumn > 1
+    )
+      dNeighbors.topLeft = [currentRow - 1, currentColumn - 1];
+    if (
+      (path == 'main' || path == 'topRight') &&
+      currentRow > 1 &&
+      currentColumn < 8
+    )
+      dNeighbors.topRight = [currentRow - 1, currentColumn + 1];
+    if (
+      (path == 'main' || path == 'bottomRight') &&
+      currentRow < 8 &&
+      currentColumn < 8
+    )
+      dNeighbors.bottomRight = [currentRow + 1, currentColumn + 1];
+    if (
+      (path == 'main' || path == 'bottomLeft') &&
+      currentRow < 8 &&
+      currentColumn > 1
+    )
+      dNeighbors.bottomLeft = [currentRow + 1, currentColumn - 1];
+
+    for (const [key, value] of Object.entries(dNeighbors)) {
+      findRecursive(value[0], value[1], allPositions, key);
+    }
+  }
 }
